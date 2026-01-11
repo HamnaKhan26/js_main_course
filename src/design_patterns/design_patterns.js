c6403c30ef08fe23a877e0595384796ecd67acfd
HamnaKhan26 <hamnarana.faisal26@gmail.com>
2026-01-11 11:58:10 -0500

    practiced singleton design pattern

--- src/design_patterns/design_patterns.js
+++ src/design_patterns/design_patterns.js
@@ -10,13 +10,26 @@ class PubSub {
 
   subscribe(event, callback) {
     // TODO: add the callback to the subscribers
+    if(!this.subscribers[event]) {
+      this.subscribers[event] = [];
+    }
+    this.subscribers[event].push(callback);
   }
 
   publish(event, data) {
     // TODO: call the callback with the data
+    if (this.subscribers[event]) {
+      this.subscribers[event].forEach(callback => callback(data));
+    }
   }
 }
 
+const pubsub = new PubSub();
+pubsub.subscribe('event1', (data) => {
+  console.log(`event1 received with data: ${data}`);
+});
+pubsub.publish('event1', 'Hello World!'); // should log "event1 received with data: Hello World!"
+pubsub.publish('event2', 'This event has no subscribers'); // should do nothing
 /**
  * @class Singleton
  * @description A simple singleton class
@@ -25,8 +38,28 @@ class PubSub {
 class Singleton {
   constructor() {
     // TODO: return the same instance
+    if (Singleton.instance) {
+      return Singleton.instance;
+    }
+    Singleton.instance = this;
+    this.timestamp = new Date();
+    return this;
   }
+  getTimestamp() {
+    return this.timestamp;
+  }
+  getInstance() {
+    return this;
+  }
+
 }
+
+const singleton = new Singleton();
+console.log(singleton.getTimestamp());
+const singleton2 = new Singleton();
+console.log(singleton2.getTimestamp());
+
+console.log("Same instance? " + (singleton === singleton2));
 
 module.exports = {
   Singleton,
